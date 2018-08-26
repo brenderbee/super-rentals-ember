53543c764a193ffb8abc78258440eea0a46c9234
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-26 15:29:06 -0700

    Add test logic to check that clicking on .menu-about links to about page. Test 2 Passes.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,9 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import {
+  visit,
+  currentURL,
+  click
+} from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -11,6 +15,10 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+
   });
 
   test('should link to contact information.', async function (assert) {
