82013322621d910aa9797a46718ab43b44687956
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-27 09:34:44 -0700

    Add test object.

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -2,25 +2,28 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | rental-listing', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{rental-listing}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#rental-listing}}
-        template block text
-      {{/rental-listing}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  hooks.beforeEach(function() {
+    this.rental = EmberObject.create({
+      image: 'fake.png',
+      title: 'test-title',
+      owner: 'test-owner',
+      type: 'test-type',
+      city: 'test-city',
+      bedrooms: 3
+    });
   });
+
+  test('should display rental details', async function(assert) {
+
+  });
+
+  test('should toggle wide class on click', async function(assert) {
+
+  });
+
 });
